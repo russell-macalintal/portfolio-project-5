f27b2abb3eee7a3f330af24375258fd4578cd320
rumacalin <russell.macalintal@gmail.com>
2021-10-08 23:56:27 -0400

    Added GameIdeas containers and components

--- game-ideas/src/containers/GameIdeasContainer.js
+++ game-ideas/src/containers/GameIdeasContainer.js
@@ -1,5 +1,18 @@
 import React, { Component } from 'react';
+import GameIdeasInput from '../components/ideas/GameIdeasInput';
+import GameIdeas from '../components/ideas/GamesIdeas';
 
 export default class GameIdeasContainer extends Component {
+    componentDidMount() {
+        // code to fetch existingn GameIdeas and Comments from Rails API
+    }
 
+    render() {
+        return (
+            <div>
+                <GameIdeasInput />
+                <GameIdeas />
+            </div>
+        )
+    }
 }
