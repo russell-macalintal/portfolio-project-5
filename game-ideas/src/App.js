a498657701ff0a7895d8ff17a6a01ba61a3795a3
rumacalin <russell.macalintal@gmail.com>
2021-10-10 22:00:22 -0400

    Add basic navigation bar

--- game-ideas/src/App.js
+++ game-ideas/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
 import Home from './components/Home';
 import About from './components/About';
 import GameIdeasContainer from './containers/GameIdeasContainer';
@@ -11,6 +12,7 @@ class App extends Component {
     return (
       <Router>
         <div>
+          <NavBar />
           <Route exact path="/" component={Home} />
           <Route exact path="/about" component={About} />
           <Route path="/game-ideas" render={(routerProps) => <GameIdeasContainer {...routerProps} />} />
---
+++ game-ideas/src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const NavBar = () => {
+    return(
+        <div>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/about">About</NavLink>
+            <NavLink to="/game-ideas">Game Ideas</NavLink>
+        </div>
+    )
+}
+
+export default NavBar;
